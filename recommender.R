13330ea53dd567d6c15837c6632bb70a3b238b38
Kaley <kaley.hanrahan@gmail.com>
2017-05-07 19:27:02 -0400

    Recommender system without function

--- recommender.R
+++ recommender.R
@@ -1,5 +1,7 @@
 library(vegan)
 library(bnlearn)
+library(magrittr)
+library(dplyr)
 ########################
 ## Recommender System ##
 ########################
@@ -25,7 +27,7 @@ df <- df[,c(11:99)]
 
 # change all column types
 for (i in 1:ncol(df)){
-  df[,i] <- as.numeric(df[,i])
+  df[,i] <- as.factor(df[,i])
 }
   
 res <- hc(df)
@@ -47,3 +49,36 @@ print(fittedbn$New.York.NY)
 # to narrow down the events, find top 5 shortest distances from matrix above to first city
 # Example: top 5 closest to NY:
 # LA, Philly, etc
+
+## Recommender system (without function)
+##########################################
+i <- 509 # Set the row number for the transaction of interest
+
+i_cities <- names(df)[df[i, 1:ncol(df)]=="1"]
+i_distDF <- dist_df[rownames(dist_df) %in% i_cities, !(names(dist_df) %in% i_cities)]
+i_top5 <- colSums(i_distDF) %>%
+  sort() %>%
+  head(5) %>%
+  names()
+
+probs <- data.frame(city=i_top5, similar_rank=(1:length(i_top5)), prob=0)
+
+for (y_city in i_top5){
+  str = paste("(", y_city, "==", 1, ")", sep = "")
+  prob_city <- cpquery(fittedbn, eval(parse(text = str)), evidence=as.list(df[i, !(names(df) %in% y_city)]), method="lw")
+  
+  probs$prob[probs$city==y_city] <- prob_city
+}
+
+i_cities
+arrange(probs, desc(prob))
+
+
+## References
+##########################################
+
+# Collaborative filtering overview
+#http://ijcai13.org/files/tutorial_slides/td3.pdf
+
+# Simply BN classifier
+#https://www.cs.rutgers.edu/~pazzani/Publications/koji.pdf
